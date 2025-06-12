069592f00349839497d669bdf496878bee8d1d7a
Jack Zhao <jack@foreflight.com>
2025-06-12 11:11:35 -0500

    example app updates and cleanup, including in the xcodeproj

---
+++ Examples/CounterApp/CounterAppTests/CounterAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  CounterAppTests.swift
+//  CounterAppTests
+//
+//  Created by Jack Zhao on 6/12/25.
+//
+
+import Testing
+
+struct CounterAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
