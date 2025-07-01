98f5e5a9e22aed21ffdcd6f6414a95209895e1cc
Jack Zhao <jack@foreflight.com>
2025-07-01 16:54:34 -0500

    implement binding helpers

--- Sources/SwiftFlux/Store/Binder.swift
+++ Sources/SwiftFlux/Store/Binder.swift
@@ -5,13 +5,97 @@
 //  Created by Jack Zhao on 7/1/25.
 //
 
-import Foundation
+import SwiftUI
 
 @MainActor
-public protocol Binder: Sendable {
-    var store: Store { get }
-}
+public protocol Binder: Selector, Dispatcher {}
 
 extension Binder {
+    public func bind<State>(_ state: State, _ action: @escaping (State) -> some Action) -> Binding<State> {
+        Binding {
+            state
+        } set: { newValue in
+            dispatch(action(newValue))
+        }
+    }
 
+    public func bind<Selection: SwiftFlux.Selection>(_ selection: Selection, _ action: some Action) -> Binding<Selection.State> {
+        Binding {
+            store.select(selection)
+        } set: { newValue in
+            dispatch(action)
+        }
+    }
+
+    public func bind<Selection: SwiftFlux.Selection>(_ selection: Selection, _ action: @escaping (Selection.State) -> some Action) -> Binding<Selection.State> {
+        Binding {
+            store.select(selection)
+        } set: { newValue in
+            dispatch(action(newValue))
+        }
+    }
+
+    public func bind<State>(_ keyPath: KeyPath<Store, State>, _ action: some Action) -> Binding<State> {
+        Binding {
+            store.select(KeyPathSelection(keyPath: keyPath))
+        } set: { newValue in
+            dispatch(action)
+        }
+    }
+
+    public func bind<State>(_ keyPath: KeyPath<Store, State>, _ action: @escaping (State) -> some Action) -> Binding<State> {
+        Binding {
+            store.select(KeyPathSelection(keyPath: keyPath))
+        } set: { newValue in
+            dispatch(action(newValue))
+        }
+    }
+
+    public func bind<State: Sendable>(_ type: State.Type, _ action: some Action) -> Binding<State> {
+        Binding {
+            store.select(StateTypeSelection())
+        } set: { newValue in
+            dispatch(action)
+        }
+    }
+
+    public func bind<State: Sendable>(_ type: State.Type, _ action: @escaping (State) -> some Action) -> Binding<State> {
+        Binding {
+            store.select(StateTypeSelection())
+        } set: { newValue in
+            dispatch(action(newValue))
+        }
+    }
+
+    public func bind<SharedState: Sendable, State>(_ map: @escaping (SharedState) -> State, _ action: some Action) -> Binding<State> {
+        Binding {
+            store.select(StateMapSelection(map: map))
+        } set: { newValue in
+            dispatch(action)
+        }
+    }
+
+    public func bind<SharedState: Sendable, State>(_ map: @escaping (SharedState) -> State, _ action: @escaping (State) -> some Action) -> Binding<State> {
+        Binding {
+            store.select(StateMapSelection(map: map))
+        } set: { newValue in
+            dispatch(action(newValue))
+        }
+    }
+
+    public func bind<SharedState: Sendable, State>(_ keyPath: KeyPath<SharedState, State>, _ action: some Action) -> Binding<State> {
+        Binding {
+            store.select(StateKeyPathSelection(keyPath: keyPath))
+        } set: { newValue in
+            dispatch(action)
+        }
+    }
+
+    public func bind<SharedState: Sendable, State>(_ keyPath: KeyPath<SharedState, State>, _ action: @escaping (State) -> some Action) -> Binding<State> {
+        Binding {
+            store.select(StateKeyPathSelection(keyPath: keyPath))
+        } set: { newValue in
+            dispatch(action(newValue))
+        }
+    }
 }
--- Sources/SwiftFlux/Store/Store+EnvironmentKey.swift
+++ Sources/SwiftFlux/Store/Store+EnvironmentKey.swift
@@ -5,17 +5,15 @@
 //  Created by Jack Zhao on 6/30/25.
 //
 
-#if canImport(SwiftUI)
-    import SwiftUI
+import SwiftUI
 
-    extension Store: EnvironmentKey {
-        public static nonisolated let defaultValue: Store = Store.build()
-    }
+extension Store: EnvironmentKey {
+    public static nonisolated let defaultValue: Store = Store.build()
+}
 
-    extension EnvironmentValues {
-        public var store: Store {
-            get { self[Store.self] }
-            set { self[Store.self] = newValue }
-        }
+extension EnvironmentValues {
+    public var store: Store {
+        get { self[Store.self] }
+        set { self[Store.self] = newValue }
     }
-#endif
+}
